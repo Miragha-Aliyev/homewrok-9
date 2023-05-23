27cca7c3d02fab47d43b34fc3eef93db9b07f6e3
unknown <Miragha-Aliyev>
2023-05-23 14:30:07 +0400

    ev tapshiriqi 9

---
+++ main.js
@@ -0,0 +1,61 @@
+// ===============================================================================================================================
+// ======== istifadeciden bir cumle daxil etmesini iste. daha sonra cumlede olan butun sozleri bir array-a cevir =================
+// ===============================================================================================================================
+
+
+// let userName = prompt ("Please enter your text")
+
+// let text = userName.split(" ")
+
+// console.log(text)
+
+
+// ================================================================================================================================
+// ======== iki eded ededler array-i ni birlesdir daha sonra onlarin icinde olan 2 reqemli ededleri ekrana yazdir =================
+// ================================================================================================================================
+
+
+// let myNumbers1 = [555, 20, 108, 214, 15, 255, 75]
+// let myNumbers2 = [140, 17, 11, 18, 777, 400, 12]
+
+// let allNumbers = myNumbers1.concat(myNumbers2)
+
+
+// for (let i = 0; i < allNumbers.length; i++){
+
+//     if (allNumbers[i]>9 && allNumbers[i]<100){
+        
+//         console.log(allNumbers[i])
+//     }
+
+// }
+
+// console.log(allNumbers)
+
+
+
+// =========================================================================================================================================
+// ======== array icinde olan sozlerin her birinin ilk 3 herfini saxliyan ve onlari yeni bir arrayda qaytaran funksiya yaz =================
+// =========================================================================================================================================
+
+
+// let myWords = "kitab, qelem, defter"
+// let newMyWords = myWords.slice(0, 3)
+// let newMyWords1 = myWords.slice (7, 10) 
+// let newMyWords2= myWords.slice(14, 17)
+
+// console.log("newMyWords, newMyWords1, newMyWords2")
+
+
+
+// let myWords = "kitab"; "qelem"; "defter"
+
+// let newMyWords = myWords.slice(0, 3)
+
+
+// console.log(newMyWords)
+
+
+
+
+
